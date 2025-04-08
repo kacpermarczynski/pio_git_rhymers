52c3b4336f7c8e500112af2103a4af944eeb87c0
kacpermarczynski <252814@edu.p.lodz.pl>
2025-04-08 21:32:58 +0200

    17. Wygenerowanie szkieletu dokumentacji dla klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,16 +1,28 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa realizuje stos FIFO, dziedziczy ona DefaultCountingOutRhymer
+ * Implementuje metode countOut(), ktora usuwa elementy takiej kolejnosci, w jakiej zostaly one dodane
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /** Tymczasowy stos uzywany do realizacji FIFO */
     public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Nadpisana metoda countOut(), ktora usuwa elementy ze stosu w kolejności FIFO
+     * @return usunięty element lub -1, jesli stos byl pusty.
+     */
     @Override
     public int countOut() {
+        // Przenosi elementy do tymczasowego stosu
         while (!callCheck())
             temp.countIn(super.countOut());
 
+        // Usuwa element z tymczasowego stosu
         int ret = temp.countOut();
 
+        // Przenosi elementy z powrotem na oryginalny stos
         while (!temp.callCheck())
             countIn(temp.countOut());
 
